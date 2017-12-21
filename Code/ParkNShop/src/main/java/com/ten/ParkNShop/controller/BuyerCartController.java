0c604622b61944d216aec5d0baee41edef8d565f
DESKTOP-GU2LJBE\Xudong <807084394@qq.com>
2017-12-21 22:13:22 +0800

    更新了BuyerCartController.java

--- Code/ParkNShop/src/main/java/com/ten/ParkNShop/controller/BuyerCartController.java
+++ Code/ParkNShop/src/main/java/com/ten/ParkNShop/controller/BuyerCartController.java
@@ -6,15 +6,19 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ten.ParkNShop.entity.Buyer;
 import com.ten.ParkNShop.entity.BuyerCart;
+import com.ten.ParkNShop.entity.Product;
+import com.ten.ParkNShop.mapper.ProductMapper;
 import com.ten.ParkNShop.service.BuyerCartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Constants;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
@@ -28,6 +32,33 @@ import java.io.IOException;
 public class BuyerCartController {
     @Autowired
     private BuyerCartService buyerCartService;
+    private ProductMapper productMapper;
+    @RequestMapping("/AddToCart")
+    public int addToCart(HttpServletRequest request, HttpServletResponse response) {
+        int productId = Integer.parseInt(request.getParameter("productId"));
+        int productNumber = Integer.parseInt(request.getParameter("producctNumber"));
+        HttpSession session = request.getSession();
+        BuyerCart buyerCart = (BuyerCart) session.getAttribute("cart");
+        if (buyerCart == null){
+            buyerCart = new BuyerCart();
+        }
+        Product product = productMapper.findByProductId(productId);
+        buyerCart.addProduct(product,productNumber);
+        return productId;
+    }
+
+    @RequestMapping("/DelFromCart")
+    public int delFromCart(HttpServletRequest request, HttpServletResponse response) {
+        int productId = Integer.parseInt(request.getParameter("productId"));
+        HttpSession session = request.getSession();
+        BuyerCart buyerCart = (BuyerCart) session.getAttribute("cart");
+        if (buyerCart==null){
+            return 0;
+        }
+        Product product = productMapper.findByProductId(productId);
+        buyerCart.deleteProduct(product);
+        return productId;
+    }
 
     @RequestMapping("/ViewCart")
     public String ViewCart(Model model,HttpServletRequest request, HttpSession session) throws IOException {
@@ -35,4 +66,6 @@ public class BuyerCartController {
         model.addAttribute("buyerCart",buyerCartService.getBuyerCart(request,session));
         return "Buyer/BuyerCart";
     }
+
+
 }

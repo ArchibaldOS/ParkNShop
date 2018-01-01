ff64ca7bc3c6d1944f1211f5162b0a007cac6afc
DESKTOP-GU2LJBE\Xudong <807084394@qq.com>
2018-01-02 00:08:22 +0800

    修改了BuyerReturnsController.java

--- Code/ParkNShop/src/main/java/com/ten/ParkNShop/controller/BuyerReturnsController.java
+++ Code/ParkNShop/src/main/java/com/ten/ParkNShop/controller/BuyerReturnsController.java
@@ -3,6 +3,7 @@ package com.ten.ParkNShop.controller;
 import com.ten.ParkNShop.service.BuyerReturnsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class BuyerReturnsController {
     @Autowired
@@ -12,10 +13,10 @@ public class BuyerReturnsController {
         return "Buyer/buyerReturns";
     }
     @RequestMapping("/buyerReturnsConfirm")
-    public String buyerReturnsConfirm(int orderId,int value){
-        if (value == 0){
+    public String buyerReturnsConfirm(int orderId,@RequestParam String action){
+        if (action.equals("RefundOnly")){
             buyerReturnsService.refundOnly(orderId);
-        }else {
+        }else if (action.equals("ReturnAndRefund")){
             buyerReturnsService.returnAndRefund(orderId);
         }
         return "forward:/viewMyOrdersClick";
